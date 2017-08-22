385078752348aeafa9c49cec1d86e7f66863a1f7
Kravchuk <majerkravchuk@gmail.com>
2017-08-22 10:49:29 +0300

    Implement interact with background.js from the popup

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,7 @@
 chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action == "toGoogle") {
     chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.executeScript(tab.id, { file: 'scripts/searcher.js' });
       console.log(tab);
     });
   }
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    if (tabs[0].url.indexOf('www.upwork.com/jobs/') == -1) {
+      $("#toGoogle").prop('disabled', true);
+    }
+  });
+
   $("#toGoogle").click(function () {
     chrome.extension.sendMessage({ action: "toGoogle" } );
+    window.close();
   });
 });
-
-
-
-
-
-
-
-
-
