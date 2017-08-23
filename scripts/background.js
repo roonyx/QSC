6388bb48b9e15670fb7fd4914cfa4a47bb8cc0c2
Kravchuk <majerkravchuk@gmail.com>
2017-08-23 10:40:04 +0300

    Refactoring background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,17 +1,24 @@
 chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action == "parsePage") {
-    chrome.tabs.getSelected(null, function(tab) {
-      chrome.tabs.executeScript(tab.id, { file: 'scripts/searcher.js' });
-    });
-  } else if (request.action == "toGoogle") {
-    var name = request.data.buyer.info.company.name;
-    var city = request.data.buyer.info.location.city;
-    var country = request.data.buyer.info.location.country;
-    var searchRequest = searchRequest = name + ' ' + (city || country);
-
-    chrome.tabs.create({
-      active: false,
-      url: 'http://www.google.com/search?q=' + searchRequest.split(' ').join('+')
-    });
-  }
+  window[request.action](request, sender, sendResponse);
 });
+
+function parsePage(request, sender, sendResponse) {
+  chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.executeScript(tab.id, { file: 'scripts/searcher.js' });
+  });
+}
+
+function toGoogle(request, sender, sendResponse) {
+  var name = request.data.buyer.info.company.name;
+  var city = request.data.buyer.info.location.city;
+  var country = request.data.buyer.info.location.country;
+
+  var searchRequest = [name, city, country]
+    .filter(i => typeof i === 'string')
+    .join('+');
+
+  chrome.tabs.create({
+    active: false,
+    url: 'http://www.google.com/search?q=' + searchRequest
+  });
+}
